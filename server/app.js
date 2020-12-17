88ed9e8e5222ae091c70753bac3ecee3a67608b1
gr3gdev <gregory.tardivel@gmail.com>
2020-12-17 13:37:59 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -6,14 +6,14 @@ const bodyParser = require('body-parser')
 const notesRoutes = require('./routes/notes')
 const usersRoutes = require('./routes/users')
 
-app.use(express.static(`${__dirname}/client`))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.use('/api/notes', notesRoutes)
 app.use('/api/users', usersRoutes)
-app.use('*', (req, res) => {
-    res.sendFile(`${__dirname}/client/index.html`)
+app.use('/*', (req, res) => {
+    const path = req.params[0] ? req.params[0] : 'index.html'
+    res.sendFile(path, {root: './client'});
 })
 
 module.exports = app
